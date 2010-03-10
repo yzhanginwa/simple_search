2453c63328bfb83585172683b1748680598bdeda
Yi Zhang <yzhang.wa@gmail.com>
2010-03-10 02:06:14 -0800

    Form fields names now needs to be "association_name.column" or just "column" for the result table. Used AssociationReflection to generate the join clause Used the Column class to figure out if a column is of number

--- lib/simple_search.rb
+++ lib/simple_search.rb
@@ -1,5 +1,7 @@
 class SimpleSearch 
-  def initialize(criteria, config={})
+  def initialize(model_class, criteria, config={})
+    @model_class = (model_class.is_a?(Symbol) || model_class.is_a?(String))? model_class.to_s.capitalize.constantize : model_class
+    @table_name = @model_class.table_name
     @criteria = criteria.nil? ? {} : criteria
     sanitize_criteria
     @config = {:exact_match => []}
@@ -31,18 +33,27 @@ class SimpleSearch
   def conditions
     return @conditions unless @conditions.nil? 
     @criteria.each do |key, value|
-      insert_condition(key, value)
-      insert_join(key, value)
+      table, field, ass_ref = parse_attribute(key)
+      insert_condition(table, field, ass_ref, value)
+      insert_join(table, field, ass_ref, value)
     end
     @conditions
   end
 
+  def run(option={})
+    @model_class.all({:select => "distinct #{@model_class.table_name}.*", :conditions => self.conditions, :joins => self.joins}.merge(option))
+  end
 
   private
 
-  def insert_condition(key, value)
+  def insert_condition(table, field, ass_ref, value)
+    key = "#{table}.#{field}"
+
     @conditions ||= []
-    if !value.is_a?(String) || @config[:exact_match].include?(key)
+    model_class = ass_ref.nil? ? @model_class : ass_ref.klass
+    column = model_class.columns_hash[field.to_s]
+
+    if column.number? || @config[:exact_match].include?((@table_name == table)? field : key)
       verb = '='
     else
       verb = 'like'
@@ -58,13 +69,33 @@ class SimpleSearch
     end
   end     
 
-  def insert_join(key, value)
-    if key =~ /\./
-      table, field = key.split(/\./)
-      if @join_mappings[table] && @joins[table].nil?
-        @joins[table] = @join_mappings[table]
+  def insert_join(table, field, ass_ref, value)
+    unless table == @table_name
+      if @joins[table].nil?
+        if ass_ref.belongs_to?
+          @joins[table] = "#{@table_name}.#{ass_ref.primary_key_name} = #{table}.#{ass_ref.klass.primary_key}"
+        else
+          @joins[table] = "#{@table_name}.#{@model_class.primary_key} = #{table}.#{ass_ref.primary_key_name}"
+        end
       end
     end
+  end
+
+  def parse_attribute(attribute)
+    if attribute =~ /\./
+      association, field = attribute.split(/\./)
+    else
+      association, field = nil, attribute
+    end
+    
+    if association.nil?
+      association_reflection = nil
+      table = @model_class.table_name
+    else
+      association_reflection = @model_class.reflect_on_association(association.to_sym)
+      table = association_reflection.klass.table_name    
+    end
+    [table, field, association_reflection]
   end
 
   def sanitize_criteria
